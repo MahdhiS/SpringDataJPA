3c74ca865d2f413be8b01d2ff39c32c59ef69a17
Mahdhi S <mahdhi.dev@gmail.com>
2025-07-01 01:37:48 +0530

    Connected to Database and Created Student.java class

---
+++ src/main/java/com/example/springdatajpa/entity/Student.java
@@ -0,0 +1,29 @@
+package com.example.springdatajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="tbl_student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(name = "email_address",unique=true, nullable=false)
+    private String emailId;
+
+    @Embedded
+    private Guardian guardian;
+}
